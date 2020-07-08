7257c7e56048d18fe592d1b1032866b6d10ae14b
Cameron <63620755+TenMinuteAdRevenue@users.noreply.github.com>
2020-07-08 18:41:05 -0400

    Update init.lua

--- Gif/init.lua
+++ Gif/init.lua
@@ -49,11 +49,11 @@ function gif:Start()
             if self.Tick + dt >= self.UpdateSpeed then
                 self.Tick = 0
 
-                self.Frame = math.clamp(
-                    self.Frame + 1,
-                    1,
-                    #self.FrameData
-                )
+                if self.Frame + 1 >= #self.FrameData then
+                    self.Frame = 1
+                else
+                    self.Frame = self.Frame + 1
+                end;
             else
                 self.Tick = self.Tick + dt
             end;
